297dfa79988014f6e1ee416650ab74964c3b7863
AyonHasan07 <rh.rakibulhasan77@gmail.com>
2024-02-13 18:46:46 +0600

    javascipt for rhombus done

--- first.js
+++ first.js
@@ -19,6 +19,24 @@ function calculateRectangle(){
     setArea('rectangle-area', area);
 }
 
+function calculateParallelogram(){
+    const base = findValueById('parallelogram-b');
+    const height = findValueById('parallelogram-h');
+
+    const area = base * height ;
+
+    setArea('parallelogram-area', area);
+}
+
+function calculateRhombus(){
+    const diagonal1 = findValueById('rhombus-d1');
+    const diagonal2 = findValueById('rhombus-d2');
+
+    const area = 0.5 * diagonal1 * diagonal2;
+
+    setArea('rhombus-area', area);
+}
+
 function findValueById(element){
     const inputField = document.getElementById(element);
     const inputValue = inputField.value;
